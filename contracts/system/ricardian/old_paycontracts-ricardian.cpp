4b228ddeaa08ae166e185b4d99e8040832e23c34
James-Mart <jmartx93@gmail.com>
2022-01-28 17:59:25 +0000

    Remove old ricardian files

--- contracts/system/ricardian/old_paycontracts-ricardian.cpp
+++
@@ -1,8 +0,0 @@
-#include "paycontracts.hpp"
-
-const char* system_epn::addasset_ricardian = R"(
-This action adds a new asset as an official token supported by the EPN.
-)";
-
-const char* system_epn::ricardian_clause = R"(
-)";
